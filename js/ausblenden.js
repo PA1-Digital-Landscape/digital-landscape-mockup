72a6850f3de6eab536cfba8e48357e379051047a
ladarsan <laila.darsane@outlook.com>
2017-07-06 17:51:04 +0200

    Bearbte Landingpage und Startseite

--- js/ausblenden.js
+++ js/ausblenden.js
@@ -1,6 +1,36 @@
+
+// ****************** Startseite *****************// 
+
+$(document).ready(function() {
+  $('#content').hide();
+	$('#link_oakmania').click(function(){
+    	$('#Oakmania').show();
+    })
+});
+
+
+
+
+
+
+
+// ****************** Projekte *****************// 
+
+
 $(document).ready(function() {
   $('#Oakmania').hide();
 	$('#link_oakmania').click(function(){
     	$('#Oakmania').show();
     })
 });
+
+$(document).ready(function() {
+  $('#Oakmania_P').hide();
+	$('.Praesentation_button').click(function(){
+    	$('#Oakmania_P').show('');
+    })
+});
+
+
+
+
