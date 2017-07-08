4065f889a49b46710a44a056d180f40facc3426f
cetranxx <Celina.Tran@gmx.de>
2017-07-08 13:37:34 +0200

    Hinzufügen von Js-Code

---
+++ js/showhide.js
@@ -0,0 +1,13 @@
+$('#show').click(function()
+{
+	$('#show').css('display','none');
+	$('#data').show();
+	§('#hide').show();
+});
+
+$('#hide').click(function()
+{
+	$('#hide').css('display','none');
+	$('#data').hide();
+	§('#show').show();
+});
