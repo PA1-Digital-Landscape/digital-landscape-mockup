5bf3b17e7f998581fea19e514e79e3c588e6e9f9
cetranxx <Celina.Tran@gmx.de>
2017-07-08 13:52:20 +0200

    In Dollarzeichen ändern

--- js/showhide.js
+++ js/showhide.js
@@ -2,12 +2,12 @@ $('#show').click(function()
 {
 	$('#show').css('display','none');
 	$('#data').show();
-	§('#hide').show();
+	$('#hide').show();
 });
 
 $('#hide').click(function()
 {
 	$('#hide').css('display','none');
 	$('#data').hide();
-	§('#show').show();
+	$('#show').show();
 });
